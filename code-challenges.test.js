d5290ee5b8fad1d8c756939d7b19f6e91ad0d72b
iraholmes <ira.holmes63@gmail.com>
2023-04-03 18:57:25 -0700

    all questions completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
 
+const { number } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -12,35 +14,168 @@
 
 // --------------------1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
 
+// PsuedoCode
 // a) Create a test with an expect statement using the variable provided.
+// Create a function called KeyValues that takes in the array of objects and returns an array with a sentence about each person with their name capitalized.
+// Create a parameter called sentences that will be used to iterate through the array
+// use the method .map() to iterate through the arrays (value) at the objects: name allowing me to return a new array with the first letter of the name capitalized and add a sentence.
+// I will need to separate the letters in string of the name-object 
+// Create a new array to place the newly broken up string allowing me to move to the next step.
+// I will use .map to iterate through the new array loooking for the first and last name
+// Push the value of the words into the empty array and use the index of the first letter to capitalize the first letter of the first and last name
+// Add the now separated strings together with addition and return the expected output using string interpolation.
+describe("keyValues", () => {
+  it("returns an array with a sentence about each person with their name capitalized", () => {
+    expect(keyValues(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
+
 
 const people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "a radio employee" }
 ]
+// First Test Outcome:  ReferenceError: keyValues is not defined
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
-
+//Working Code!!
 // b) Create the function that makes the test pass.
+const keyValues = (sentences) =>{
+      return sentences.map(value =>  {
+        const capitalLetter = value.name.split(" ")
+        let emptyArray = []
+         capitalLetter.map(word => {
+          return emptyArray.push(word[0].toUpperCase() + word.slice(1)) 
+      })
+          return `${emptyArray.join(" ")} is ${value.occupation}.`
+  })
+}
+console.log(keyValues(people));
+
+// BELOW IS THE THOUGHT PROCESSE BECFORE COMING TO MY FINAL CONCLUSION
+
+// if (value.name.includes("f,p,z,a,d")) {
+//Testing Code
+// const keyValues = (sentences) =>{
+//   let value = sentences
+//   return sentences.map(value => {
+//     for (let i = 0; i < value.length; i++) {
+//       value.name[i] = value.name[i][0].toUpperCase() + value.name[i].substr(1);
+//       return `${value.name} is a ${value.occupation}`
+// }
+
+
+
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
+// PsuedoCode: 
 // a) Create a test with an expect statement using the variables provided.
+// Create a function named newArray that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3
+// find the method tht will filter out all integers and allow me to iterate over them.
+// find the remainder of the integers in the array by using the % 3 === 0 modulous.
+// right the return that will give me the expected output of [ 2, 0, -1, 0 ] & [ 2, 1, -1 ]
+// Determine whether or not to use .map/ OR .filter/
+// using Type of I can target only the numbers in the array
+// Using .filter I can extract the numbers from the array then use .map to iterate through the array and return a new array that holds the number values of the previous array
+// I will need to return the values of the array divided by 3
+describe("newArray", () => {
+  it("returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+    expect(newArray(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(newArray(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+
 
 const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+// First Outcome: ReferenceError: newArray is not defined
 // b) Create the function that makes the test pass.
+const newArray = (numbers) => {
+  return numbers.filter((value) => {
+  return typeof value === "number"
+  }).map((value) => {
+    return value % 3 
+  })
+
+}
+console.log(newArray(hodgepodge1));
+// COMMENTED OUT CODE ARE DIFFERENT IDEAS BEFORE COMING ACCROSS MY CONCLUSION
+// const newArray = (numbers) => {
+//     return numbers.map(value =>  {
+//     const divided = value <= 50
+//     let newNum =[]    
+//     return newNum.push(divided % 3)
+//     })
+//   }
+
+// const newArray = (array) => {
+//     return arraynumber.isInteger(value => {
+//       let arr = []
+//     const newMath = (number.isInteger(value) === true) 
+//         return arr.push(array % 3)
+//       })
+//     }
+//  Thought proccess is to get the numbers (filtered out of the array and then add them to a new array and iterate over them.)
+// const newArray = (array) => {
+//   return  array.map(value => {
+//     const doOver = value(number.isInteger(array))
+//     const leftOver = []
+//     doOver.map(math => {
+//     return (array % 3)
+
+//     })
+//   })
+// }
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
-
+// PSUEDO CODE: 
 // a) Create a test with an expect statement using the variables provided.
+// I will create a function that will take in the arrays given. titles addedUp
+// Use the paraemeter total to signify those arrays
+// current array will hold my total varable and use the .map method to iterate through the arrays while storing them inside of the currentArray variable
+// My first return will cube the values of the array
+// create a variable to hold the potential sum of the values in the given array
+// use the forEach method to perform addition on each instance inside of the value array.
+// return the total sum of the array after it has been cubed.
+describe("addedUp", () => {
+  it("returns a generic greeting", () => {
+    expect(addedUp(cubeAndSum1)).toEqual(99)
+    expect(addedUp(cubeAndSum2)).toEqual(1125)
+  })
+})
 
+
+
+// ReferenceError: addedUp is not defined
 const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
 // b) Create the function that makes the test pass.
+const addedUp = (total) => {
+  const currentArray = total.map(value => {
+    return (value ** 3)
+  })
+  let sum = 0 
+ currentArray.forEach (x => {
+    sum += x
+  }) 
+  return sum
+}
+  console.log(addedUp(cubeAndSum1));
+  // const allnums = (addEm) => {
+  //   return (num ** 3 + total)
+  // }
+  // const addedUp = (total) => {
+  //   for (let i = 0; i < total.length; i++) {
+  //     return (total[i] ** 3)
+  //   }
+  // }
+  // console.log(addedUp(cubeAndSum1))
+  // console.log(addedUp(cubeAndSum2))
+  //Math is working, figure out why only the first number is being iterated on.
--- interview-questions.md
+++ interview-questions.md
@@ -8,36 +8,61 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-Your answer:
+Your answer: The foreign key would be known as the primary key on the Cohort Model and when inserted into the student model will act as a foreign key (relating the data between the two models together) This mistake can be fixed by updating the student model to include a new column named after the primary key column in the Cohort model since that is the original database model that holds the starting information. 
 
-Researched answer:
+Researched answer: Primary keys are unique identifiers they also help to identify specific records without allowing duplications, They are usually number based such as driver's license number or customer ID numbers. The foreign key's purpose is to link the two tables together (there can be more than one and they can also be null) the link between a foreign key column and another tables linked column cannot be destroyed due to constraints placed on the foreign key by the system.
 
 2. Which RESTful routes must always be passed params? Why?
 
-Your answer:
+Your answer: I believe Update, edit, show and Destroy( otherwise known as patch, Get, delete ) because they will rely on the params passed to know where they need to make the changes to your application. The params give those routes a specific destination to act upon so there is no miscommunication on the data changes are being made to. 
 
-Researched answer:
+Researched answer: Params add information to a url, allowing you to store data. You can input different values and still execute a query based on the param placeholder. The RESTful routes Update, Edit, Show, and Destroy need a param to link the applications url to different actions and pages on the application while enabling different inputs to be added to request different information.
 
 3. Name three rails generator commands. What is created by each?
 
-Your answer:
+Your answer: rails g model (which will create a new model) rails g resource (which will create a backend resource database) rails g controller These are the only two I currently know
 
-Researched answer:
+Researched answer: The Rails G resource command will generate a resource database that you can essentially set up the same as a model but will create a resource routes, empty view folder and a controller.
+
+- Rails G Model - will create a migration, a model and and tests for the files generated
+- Rails G Controller - will generate methoda you would want your controller to have but will also create views for each method you input automatically 
+- Raild G -h - will generate a list of helpful commands, syntax and options that are available to the user (Super helpful never knew about this one)
+- Rails destroy - will run similarly to the other generate commands except instead og creating files it will remove them from every path
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
-action: "GET" location: /students
+action: "GET" location: /students = def index Takes you to a view page for students
 
-action: "POST" location: /students
+action: "POST" location: /students = def create - will alow you to create a student in the application
 
-action: "GET" location: /students/new
+action: "GET" location: /students/new = def new - will take you to a view page for the creation of a new student (GET Read Command will show you the information on the page)
 
-action: "GET" location: /students/2
+action: "GET" location: /students/2 = def show - most likely will have a .find(params[:id]) and will take you to view a page with student id:2 information.
 
-action: "GET" location: /students/2/edit
+action: "GET" location: /students/2/edit = def edit - will take you to a view page for editing student with id:2's information. (This is a put action I believe) 
 
-action: "PATCH" location: /students/2
+action: "PATCH" location: /students/2 = def update - this will allow you to submit updated changes from editing student with id:2's information
 
-action: "DELETE" location: /students/2
+action: "DELETE" location: /students/2 = def destroy - Will delete the information of student at id:2
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
+1 - As a CEO of a new start-up company for finding online deals, I want  an easy to use GUI, tso that it allows our members to easily navigate our application and find the deals that fit their shopping needs.
+
+2 - As a lawfirm manager, I would like our companies online filing system to be dynamic allowing us to store thousands of files for our clients in specific and easily findable folders in a cloud based application.
+
+3 - As a Veteran Agency, I would like to have a website friendly to senior citizens, so that they can get updates about our benefits programs more conveniently.
+
+4 - As Tom, I would like to be able to be able to upload ideas for my book into a workspace that can be accessible anywhere so I can continue where I stopped while on the go.
+
+5- As a travel agent I would like to be able to compare multiple rates together at a single time, so that I can find the best deals for my clients.
+
+6 - As a professional photographer I would like to be able to arrange many different portfolios of my work within a single app, so that I can customize them for different potential clients
+
+7 - As a musician I would like to be able to make changes to songs I am working on from my phone, so that when I am not able to work in the studio I can still get some work done.
+
+8- As a soda company CEO I would like to be able to organize the recipe for different flavors into a safe dynamic file sharing system, so that I can pass updates to my co-workers
+
+9- As a school teacher I would like to be able to show parents how well their children did in class today from an online forum, so that we can reach out to more than one parent at a time and need less face to face encounters.
+
+10 - As a developer I want to learn the most effecient tactics in coding, so that I can code with ease and show potential employers my skills.
